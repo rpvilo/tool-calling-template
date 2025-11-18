225acecec69aba71e61a5ff246b465e2b8f3e1d3
Rasmus Viloma <rasmus.viloma@icloud.com>
2025-11-18 22:18:00 +0200

    refactor: Refactor UI components to remove forwardRef and simplify props handling

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils"
 function Skeleton({
   className,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+}: React.ComponentProps<"div">) {
   return (
     <div
       className={cn("animate-pulse rounded-md bg-primary/10", className)}
