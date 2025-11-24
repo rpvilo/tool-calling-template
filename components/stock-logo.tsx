ee3cee0116ffe669008b0f83591cc68c8b523285
Rasmus Viloma <rasmus.viloma@icloud.com>
2025-11-24 07:16:39 +0200

    feat: update StockLogo component to use dynamic theme for logo retrieval and enhance layout with a wrapper div

--- components/stock-logo.tsx
+++ components/stock-logo.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useTheme } from "next-themes";
 import type { ComponentProps } from "react";
 import { cn } from "@/lib/utils";
 
@@ -7,14 +8,17 @@ type Props = {
   symbol: string;
 } & ComponentProps<"img">;
 const StockLogo = ({ symbol, className, ...props }: Props) => {
+  const { theme } = useTheme();
   return (
-    <img
-      src={`https://cdn.brandfetch.io/${symbol}?c=1idvu27mVmcQLrcnwHx`}
-      // src={`https://img.logo.dev/ticker/${symbol}?token=pk_U3on09_rSvS4xP_cRHU4Dg`}
-      alt={symbol}
-      className={cn("size-5 rounded-sm", className)}
-      {...props}
-    />
+    <div className={cn("relative size-5 overflow-hidden rounded-sm", className)}>
+      <img
+        // src={`https://cdn.brandfetch.io/${symbol}?c=1idvu27mVmcQLrcnwHx`}
+        src={`https://img.logo.dev/ticker/${symbol}?token=pk_U3on09_rSvS4xP_cRHU4Dg&format=png&fallback=404&size=64&theme=${theme}`}
+        alt={symbol}
+        className={cn("size-full object-contain")}
+        {...props}
+      />
+    </div>
   );
 };
 
